e77cd842ba1da018f872e978d7d1f4b780d947b7
HarshaGopannola <68220355+HarshaGopannola@users.noreply.github.com>
2020-07-13 22:02:53 +0530

    assign1

---
+++ js.js
@@ -0,0 +1,13 @@
+let btnShow = document.querySelector('button');
+let output = document.quertSelector('h1');
+btnShow.addEventListener('click', () => {
+     let today = new Date();
+
+     let month = today.getMonth() + 1;
+     let year = today.getFullYear();
+     let date = today.getDate();
+
+     let current_date = '${month}/${date}/${year}';
+     output.innerText = current_date;
+
+});
